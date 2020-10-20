e03bd90b60c475ee49debf3c8d70279cd3127199
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-10-20 13:11:56 -0300

    Adding datePicker and the dialog

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,15 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import Paper from "@material-ui/core/Paper";
-
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
 import AddIcon from "@material-ui/icons/Add";
 import FilterListIcon from "@material-ui/icons/FilterList";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 
 const useStyles = makeStyles((theme) => ({}));
 
@@ -50,6 +56,10 @@ const ProjectManager = () => {
   const [iOSChecked, setIOSChecked] = useState(false);
   const [androidChecked, setAndroidChecked] = useState(false);
   const [softwareChecked, setSoftwareChecked] = useState(false);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [name, setName] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [total, setTotal] = useState("");
   const [rows, setRows] = useState([
     createData(
       "Zachary Reece",
@@ -74,118 +84,188 @@ const ProjectManager = () => {
   ]);
 
   return (
-    <Grid container direction="column">
-      <Grid item style={{ marginTop: "2em", marginLeft: "5em" }}>
-        <Typography variant="h1">Projects</Typography>
-      </Grid>
-      <Grid item>
-        <TextField
-          placeholder="Search project details or create a new entry."
-          style={{ width: "35em", marginLeft: "5em" }}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <AddIcon color="primary" style={{ fontSize: 30 }} />
-              </InputAdornment>
-            ),
-          }}
-        />
-      </Grid>
-      <Grid item style={{ marginLeft: "5em", marginTop: "2em" }}>
-        <FormGroup row>
-          <FormControlLabel
-            style={{ marginRight: "5em" }}
-            label="Websites"
-            labelPlacement="start"
-            control={
-              <Switch
-                checked={websiteChecked}
-                color="primary"
-                onChange={() => setWebsiteChecked(!websiteChecked)}
-              />
-            }
-          ></FormControlLabel>
-          <FormControlLabel
-            style={{ marginRight: "5em" }}
-            label="iOS Apps"
-            labelPlacement="start"
-            control={
-              <Switch
-                checked={iOSChecked}
-                color="primary"
-                onChange={() => setIOSChecked(!iOSChecked)}
-              />
-            }
-          ></FormControlLabel>
-          <FormControlLabel
-            style={{ marginRight: "5em" }}
-            label="Android Apps"
-            labelPlacement="start"
-            control={
-              <Switch
-                checked={androidChecked}
-                color="primary"
-                onChange={() => setAndroidChecked(!androidChecked)}
-              />
-            }
-          ></FormControlLabel>
-
-          <FormControlLabel
-            label="Custom Software"
-            labelPlacement="start"
-            control={
-              <Switch
-                checked={softwareChecked}
-                color="primary"
-                onChange={() => setSoftwareChecked(!softwareChecked)}
-              />
-            }
-          ></FormControlLabel>
-        </FormGroup>
-      </Grid>
-      <Grid item container justify="flex-end" style={{ marginTop: "5em" }}>
-        <Grid item style={{ marginRight: 75 }}>
-          <FilterListIcon
-            color="secondary"
-            style={{ fontSize: 50 }}
-          ></FilterListIcon>
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container direction="column">
+        <Grid item style={{ marginTop: "2em", marginLeft: "5em" }}>
+          <Typography variant="h1">Projects</Typography>
         </Grid>
-      </Grid>
-      <Grid item style={{ marginBottom: "15em" }}>
-        <TableContainer component={Paper} elevation={0}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">Name</TableCell>
-                <TableCell align="center">Date</TableCell>
-                <TableCell align="center">Service</TableCell>
-                <TableCell align="center">Features</TableCell>
-                <TableCell align="center">Complexity</TableCell>
-                <TableCell align="center">Platforms</TableCell>
-                <TableCell align="center">Users</TableCell>
-                <TableCell align="center">Total</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row, index) => (
-                <TableRow key={index}>
-                  <TableCell align="center">{row.name}</TableCell>
-                  <TableCell align="center">{row.date}</TableCell>
-                  <TableCell align="center">{row.service}</TableCell>
-                  <TableCell align="center" style={{ maxWidth: "5em" }}>
-                    {row.features}
-                  </TableCell>
-                  <TableCell align="center">{row.complexity}</TableCell>
-                  <TableCell align="center">{row.platforms}</TableCell>
-                  <TableCell align="center">{row.users}</TableCell>
-                  <TableCell align="center">{row.total}</TableCell>
+        <Grid item>
+          <TextField
+            placeholder="Search project details or create a new entry."
+            style={{ width: "35em", marginLeft: "5em" }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment
+                  position="end"
+                  onClick={() => setDialogOpen(true)}
+                  style={{ cursor: "pointer" }}
+                >
+                  <AddIcon color="primary" style={{ fontSize: 30 }} />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Grid>
+        <Grid item style={{ marginLeft: "5em", marginTop: "2em" }}>
+          <FormGroup row>
+            <FormControlLabel
+              style={{ marginRight: "5em" }}
+              label="Websites"
+              labelPlacement="start"
+              control={
+                <Switch
+                  checked={websiteChecked}
+                  color="primary"
+                  onChange={() => setWebsiteChecked(!websiteChecked)}
+                />
+              }
+            ></FormControlLabel>
+            <FormControlLabel
+              style={{ marginRight: "5em" }}
+              label="iOS Apps"
+              labelPlacement="start"
+              control={
+                <Switch
+                  checked={iOSChecked}
+                  color="primary"
+                  onChange={() => setIOSChecked(!iOSChecked)}
+                />
+              }
+            ></FormControlLabel>
+            <FormControlLabel
+              style={{ marginRight: "5em" }}
+              label="Android Apps"
+              labelPlacement="start"
+              control={
+                <Switch
+                  checked={androidChecked}
+                  color="primary"
+                  onChange={() => setAndroidChecked(!androidChecked)}
+                />
+              }
+            ></FormControlLabel>
+
+            <FormControlLabel
+              label="Custom Software"
+              labelPlacement="start"
+              control={
+                <Switch
+                  checked={softwareChecked}
+                  color="primary"
+                  onChange={() => setSoftwareChecked(!softwareChecked)}
+                />
+              }
+            ></FormControlLabel>
+          </FormGroup>
+        </Grid>
+        <Grid item container justify="flex-end" style={{ marginTop: "5em" }}>
+          <Grid item style={{ marginRight: 75 }}>
+            <FilterListIcon
+              color="secondary"
+              style={{ fontSize: 50 }}
+            ></FilterListIcon>
+          </Grid>
+        </Grid>
+        <Grid item style={{ marginBottom: "15em" }}>
+          <TableContainer component={Paper} elevation={0}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Name</TableCell>
+                  <TableCell align="center">Date</TableCell>
+                  <TableCell align="center">Service</TableCell>
+                  <TableCell align="center">Features</TableCell>
+                  <TableCell align="center">Complexity</TableCell>
+                  <TableCell align="center">Platforms</TableCell>
+                  <TableCell align="center">Users</TableCell>
+                  <TableCell align="center">Total</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {rows.map((row, index) => (
+                  <TableRow key={index}>
+                    <TableCell align="center">{row.name}</TableCell>
+                    <TableCell align="center">{row.date}</TableCell>
+                    <TableCell align="center">{row.service}</TableCell>
+                    <TableCell align="center" style={{ maxWidth: "5em" }}>
+                      {row.features}
+                    </TableCell>
+                    <TableCell align="center">{row.complexity}</TableCell>
+                    <TableCell align="center">{row.platforms}</TableCell>
+                    <TableCell align="center">{row.users}</TableCell>
+                    <TableCell align="center">{row.total}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Dialog
+          open={dialogOpen}
+          onClose={() => setDialogOpen(false)}
+          fullWidth
+          maxWidth="md"
+        >
+          <Grid container justify="center">
+            <Grid item>
+              <Typography variant="h1" gutterBottom>
+                Add a new project
+              </Typography>
+            </Grid>
+          </Grid>
+          <DialogContent>
+            <Grid container justify="space-between">
+              <Grid item>
+                <Grid item container direction="column" sm>
+                  <Grid item>
+                    <TextField
+                      label="Name"
+                      id="name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Grid
+                  item
+                  container
+                  direction="column"
+                  sm
+                  style={{ marginTop: 16 }}
+                >
+                  <Grid item>
+                    <KeyboardDatePicker
+                      format="MM/dd/yyyy"
+                      value={date}
+                      onChange={(newDate) => setDate(newDate)}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Grid item container direction="column" sm>
+                  <Grid item>
+                    <TextField
+                      label="Total"
+                      id="total"
+                      value={total}
+                      onChange={(e) => setName(e.target.value)}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">$</InputAdornment>
+                        ),
+                      }}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </DialogContent>
+        </Dialog>
       </Grid>
-    </Grid>
+    </MuiPickersUtilsProvider>
   );
 };
 
